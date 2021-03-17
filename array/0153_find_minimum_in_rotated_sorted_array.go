0519d3ceeaf99db348688fae91c8eb8aaea59f60
khalil <wanyxkhalil@gmail.com>
2021-03-17 13:44:52 +0800

    day

---
+++ array/0153_find_minimum_in_rotated_sorted_array.go
@@ -0,0 +1,17 @@
+package array
+
+func findMin(nums []int) int {
+	low, high := 0, len(nums)-1
+	for low < high {
+		if nums[low] < nums[high] {
+			return nums[low]
+		}
+		mid := low + (high-low)>>1
+		if nums[mid] >= nums[low] {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	return nums[low]
+}
